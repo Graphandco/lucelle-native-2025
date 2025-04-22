394e3abc103208ae0d7c95910a29ed73aa87c480
Régis Daum <rdaum@john-steel.com>
2025-04-22 14:27:49 +0200

    first commit

--- components/ScreenContent.jsx
+++ components/ScreenContent.jsx
@@ -2,16 +2,11 @@ import { Text, View } from 'react-native';
 
 import { EditScreenInfo } from './EditScreenInfo';
 
-type ScreenContentProps = {
-  title: string;
-  path: string;
-  children?: React.ReactNode;
-};
-
-export const ScreenContent = ({ title, path, children }: ScreenContentProps) => {
+export const ScreenContent = ({ title, path, children }) => {
   return (
     <View className={styles.container}>
       <Text className={styles.title}>{title}</Text>
+      <Text className="text-red-500">This is a screen content !</Text>
       <View className={styles.separator} />
       <EditScreenInfo path={path} />
       {children}
